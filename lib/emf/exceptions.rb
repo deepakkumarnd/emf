62baa6bd486fe61fce8baac44318fadae1a7f9ef
Deepak Kumar <deepak.kumar@sourcebits.com>
2012-08-13 16:03:55 +0530

    adding ; to separate single line function statements

--- lib/emf/exceptions.rb
+++ lib/emf/exceptions.rb
@@ -1,15 +1,15 @@
 module Emf
     module Exceptions
         class OptionNotFoundError < StandardError
-            def message "Error: Option should be given for details run emf --help" end
+            def message; "Error: Option should be given for details run emf --help" end
         end
 
         class DuplicateDirectoryError < StandardError
-            def message "Error: Directory already exists" end
+            def message; "Error: Directory already exists" end
         end
 
         class InvalidPathError < StandardError
-            def message "Error: Wrong path" end
+            def message; "Error: Wrong path" end
         end
     end
 end
